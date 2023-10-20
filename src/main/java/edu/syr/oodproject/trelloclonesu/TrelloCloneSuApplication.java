2799ed21645e77746e337ae606ef2bf8ff13929a
Shivakumar Suresh <shivakumarsuresh@Shivakumars-MacBook-Air.local>
2023-10-20 14:14:20 -0400

    initiliazing spring boot

---
+++ src/main/java/edu/syr/oodproject/trelloclonesu/TrelloCloneSuApplication.java
@@ -0,0 +1,13 @@
+package edu.syr.oodproject.trelloclonesu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCloneSuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloCloneSuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/syr/oodproject/trelloclonesu/TrelloCloneSuApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.syr.oodproject.trelloclonesu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloCloneSuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
